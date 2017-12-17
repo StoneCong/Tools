2ab2187ecdc3061b54373abfeb1efa0034074512
Stone Cong <stonecong@gmai.com>
2017-12-16 19:20:51 -0800

    Fine tuning the message and introduce sleep

--- teaching_kids/002.your_name.py
+++ teaching_kids/002.your_name.py
@@ -1,16 +1,20 @@
 import os
 import sys
+import time
 
 def say(message):
   if sys.platform.startswith('darwin'):
     message = 'say "%s"' % message
     os.system(message)
 
-# this will ask for your name and then print it out for you.
+# this will ask for your name and a message and speak them out
 say("What is your name?")
 name = input("What is your name: ")
 say("Hi," + name)
+
 say("What do you want me to say: ")
 message = input("message: ")
 say(message)
-say("Good Bye! Have a great time learning!")
+
+time.sleep(1)  # Wait for a second
+say("Talk to you later! Have a great time learning!")
