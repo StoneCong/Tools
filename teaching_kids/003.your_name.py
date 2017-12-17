27fc1419df663f217faa33f2fadb8d65e9884231
Stone Cong <stonecong@gmai.com>
2017-12-16 19:34:26 -0800

    Adding day 3 to include control flow

---
+++ teaching_kids/003.your_name.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import time
+
+def say(message):
+  if sys.platform.startswith('darwin'):
+    message = 'say "%s"' % message
+    os.system(message)
+
+# this will ask for your name and a message and speak them out
+say("What is your name?")
+name = input("What is your name: ")
+say("Hi," + name)
+
+say("What do you want me to say:")
+message = input("message: ")
+
+say("How many time do you want me to say the message?")
+count = input("count: ")
+
+for x in range(int(count)):
+  say(message)
+
+time.sleep(1)  # Wait for a second
+say("Talk to you later! Have a great time learning!")
